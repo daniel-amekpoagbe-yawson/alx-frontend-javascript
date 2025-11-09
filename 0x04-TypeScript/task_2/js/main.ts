4eda74b5395396923ef692b1dd75988e1e0fbe9d
daniel-amekpoagbe-yawson <amekpoagbedaniel@gmail.com>
2025-11-09 12:22:20 +0000

    tired asf

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,62 +1,3 @@
-// interface DirectorInterface {
-//   workFromHome(): string;
-//   getCoffeeBreak(): string;
-//   workDirectorTasks(): string;
-// }
-
-// interface TeacherInterface {
-//   workFromHome(): string;
-//   getCoffeeBreak(): string;
-//   workTeacherTasks(): string;
-// }
-
-// class Director implements DirectorInterface {
-//   workFromHome(): string {
-//     return "Working from home";
-//   }
-
-//   getCoffeeBreak(): string {
-//     return "Getting a coffee break";
-//   }
-
-//   workDirectorTasks(): string {
-//     return "Getting to director tasks";
-//   }
-// }
-
-// class Teacher implements TeacherInterface {
-//   workFromHome(): string {
-//     return "Cannot work from home";
-//   }
-
-//   getCoffeeBreak(): string {
-//     return "Cannot have a break";
-//   }
-
-//   workTeacherTasks(): string {
-//     return "Getting to work";
-//   }
-// }
-// function createEmployee(salary: number | string): Director | Teacher {
-//   if (typeof salary === "number") {
-//     if (salary < 500) {
-//       return new Teacher();
-//     }
-//     return new Director();
-//   }
-//   return new Director();
-// }
-
-// // ✅ Example test cases
-// console.log(createEmployee(200)); // Teacher
-// console.log(createEmployee(1000)); // Director
-// console.log(createEmployee("$500")); // Director
-
-
-
-
-
-
 interface DirectorInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
@@ -112,8 +53,7 @@ console.log(createEmployee(200)); // Teacher
 console.log(createEmployee(1000)); // Director
 console.log(createEmployee("$500")); // Director
 
-
-// ✅ New part: Task 2
+// ✅ New part (Task 2)
 type Subjects = "Math" | "History";
 
 function teachClass(todayClass: Subjects): string {
