e7fddc42d59d1be393c3b4f358cd1a6576241bb3
daniel-amekpoagbe-yawson <amekpoagbedaniel@gmail.com>
2025-11-09 12:50:46 +0000

    tired asf

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -10,12 +10,7 @@ const row: RowElement = {
 
 const newRowID: RowID = CRUD.insertRow(row);
 console.log(`New row ID: ${newRowID}`);
-
-const updatedRow: RowElement = {
-  firstName: "Guillaume",
-  lastName: "Salva",
-  age: 23,
-};
+const updatedRow: RowElement = { ...row, age: 23 };
 
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
